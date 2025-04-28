b8267891808d0c4021d9e6e18f6efc96e8495eaa
corygshava <corygshava@gmail.com>
2025-04-28 19:40:04 +0300

    added some dummy files for testing

--- index.php
+++ index.php
@@ -47,13 +47,47 @@
             color: #111;
             text-decoration: none;
         }
+
+        nav {
+            position: fixed;
+            top: 0;
+            width: 100%;
+            box-shadow: 0 0 12px rgba(0,0,0,0.3);
+            padding: 0 20px;
+            background-color: var(--bodybg);
+            color: var(--textcolor);
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+        }
+        nav .logo{
+            padding: 20px;
+            font-weight: 800;
+        }
+        nav .links a{
+            padding: 20px 12px;
+        }
+        nav .links a.current{
+            border-bottom: 2px solid var(--themecolor);
+            color: var(--themecolor);
+        }
     </style>
 </head>
 <body>
+    <nav>
+        <div class="logo">
+            <b>Uploader P*</b>
+        </div>
+        <div class="links">
+            <a href="index.php" class="current">home</a>
+            <a href="uploadfile.php">upload</a>
+            <a href="uploaded.php">files</a>
+        </div>
+    </nav>
     <div class="content">
         <div class="callout w3-center">
-            <h1>info</h1>
-            <p>a catchy tagline</p>
+            <h1>Your files anywhere, anytime</h1>
+            <p>Secure lockbox storage for all your files both big and small</p>
             <div class="w3-center">
                 <button class="themebtn active">go</button>
                 <a class="themebtn" href="uploaded.php">view uploads</a>
